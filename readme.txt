a134ab12e5f0d13a1a7d3aac1f8874991f964d87
zackdesign <zackdesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-16 03:17:27 +0000

    RSS Image Widget 1.3
    git-svn-id: https://plugins.svn.wordpress.org/rss-image-widget/tags/1.3@206308 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,82 @@
+=== RSS Image Widget ===
+Contributors: zackdesign
+http://www.zackdesign.biz/wp-plugins/40
+Tags: rss, image, feed, widget, simplepie, plugin
+Requires at least: 2.8
+Tested up to: 2.9.2
+Stable tag: 1.3
+
+This plugin provides you with a widget to display images from a feed. It requires the SimplePie plugin to be installed.
+
+== Description ==
+
+This Widget uses PHPThumb to automatically generate good-looking thumbnails from RSS feed images inside your template. These thumbnails and the feed are cached inside `wp-content/cache` so please ensure that the directory is writable.
+
+Image Feeds will work in the plugin providing that they use enclosures. If you want you can change the template provided to suit your feed if necessary, though this should work if enclosures are used correctly.
+
+I have set the default image width in the template to be 150 pixels wide. You can change or remove this in the plugin file as you like.
+
+SimplePie plugin is no longer required for this plugin as it now ships with Wordpress 2.8 and above! 
+
+Next version will include some extra options and possibly better HTML.
+
+* Link to full image or enclosure URL option
+* PHPThumb reflection option (and other options)
+* Some cache options
+* Much more
+ 
+Please be aware that I'll only be updating this if I need to. Feel free to come on board and contribute!
+
+== Screenshots ==
+
+[Zack Design Plugin Showcase](http://wordpress.zackdesign.biz "Plugin Showcase")
+
+== Installation ==
+
+1. Upload the 'rss-image-widget' folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the Presentation menu in Wordpress Admin and choose 'Widgets' in the submenu.
+4. Drag and drop the Rss Image widget onto your sidebar.
+5. Click on the widget in the form area and it should popup the form. Put your feed address in the correct spot, and specify the amount of images you want to use.
+
+
+== Changelog ==
+
+1.3 
+
+- SimplePie dependency removed as it comes with Wordpress > 2.7
+- Template dependency removed as a result of above. Now a truly plug and play solution!
+- PHPThumb changed to latest version from http://phpthumb.gxdlabs.com/
+- Now truly cachable
+
+1.2.1
+
+- Change image_widget.tmpl to validated XHTML 1.0 Transitional
+
+1.2
+
+- Uses Wordpress 2.8 widget class which means it now supports multiple instances!
+
+1.1
+
+- Addition to admin of changeable title
+
+1.0
+
+- First release
+
+== Frequently Asked Questions ==
+
+= Purple PHPThumb screen =
+
+Usually this is because you have not made PHPThumb's cache directory readable.
+
+= I Need HELP!!! =
+
+That's what I'm here for. I do Wordpress sites for many people in a professional capacity and
+can do the same for you. Check out www.zackdesign.biz
+
+= Where do I get SimplePie? =
+
+Weren't you paying attention? Read the Description again!!!
+
---
+++ rss_image.php
@@ -0,0 +1,125 @@
+<?php
+/*
+Plugin Name: RSS Images
+Plugin URI: http://www.zackdesign.biz/wp-plugins/40
+Description: RSS Image display using SimplePie
+Author: Isaac Rowntree
+Version: 1.3
+Author URI: http://zackdesign.biz
+
+	Copyright (c) 2005, 2006 Isaac Rowntree (http://zackdesign.biz)
+	QuickShop is released under the GNU General Public
+	License (GPL) http://www.gnu.org/licenses/gpl.txt
+
+	This is a WordPress plugin (http://wordpress.org).
+
+*/
+
+/**
+ * RSSImages Class
+ */
+class RSSImages extends WP_Widget {
+    /** constructor */
+    function RSSImages() {
+        parent::WP_Widget(false, $name = 'RSS Images');	
+    }
+
+    /** @see WP_Widget::widget */
+    function widget($args, $instance) {		
+        extract( $args );
+        $title = apply_filters('widget_title', $instance['title']);
+        
+	echo $before_widget; 
+        
+	if ( $title )
+            echo $before_title . $title . $after_title; 
+	
+	if (!empty($instance['url']))
+    	{
+                if (!class_exists('SimplePie'))
+		    require_once(ABSPATH.'/wp-includes/class-simplepie.php');
+		
+		$feed = new SimplePie();
+		$feed->set_feed_url($instance['url']);
+		$feed->set_cache_location( ABSPATH.'/wp-content/cache');
+		$feed->init();
+		
+		if ($instance['images'])
+		{
+		    echo '<div class="simplepie">';
+		    
+		    $count = 0;
+		    
+		    foreach ($feed->get_items() as $item)
+		    {
+			if ($count < $instance['images'])
+			{			    
+			    $count++;
+			    $url = '';
+			    
+			    if ($enclosure = $item->get_enclosure())
+	                    { 
+		                $url = $enclosure->get_link();
+				// For some reason question marks aren't happy
+				$image = str_replace("?", '', htmlspecialchars($item->get_title()).'.jpg');
+				
+				if (!file_exists(ABSPATH.'/wp-content/cache/'.$image))
+				{
+				    // PHPThumb caching
+				    require_once(ABSPATH.'/wp-content/plugins/rss-image-widget/phpthumb/ThumbLib.inc.php');
+				
+				    $thumb = PhpThumbFactory::create($url);  
+				    
+				    // Image width/height
+				    $thumb->resize(150, 150)->save(ABSPATH.'/wp-content/cache/'.$image);  		
+				}
+			    }		    
+			    
+			    echo '
+			             <div class="rss_image">
+				         <h5><a href="'.$item->get_permalink().'">'.$item->get_title().'</a></h5><br />
+                                         <a href="'.$item->get_permalink().'"><img src="'.get_bloginfo('wpurl').'/wp-content/cache/'.$image.'" alt="'.$item->get_title().'" /></a>
+				    </div><br />';
+			}
+			else
+			    break;
+		    }
+		    
+		    echo '</div>';
+		}
+	}
+        
+	echo $after_widget; 
+    }
+
+    /** @see WP_Widget::update */
+    function update($new_instance, $old_instance) {				
+        return $new_instance;
+    }
+
+    /** @see WP_Widget::form */
+    function form($instance) {				
+        $title = esc_attr($instance['title']);
+	$url = esc_attr($instance['url']);
+	$images = esc_attr($instance['images']);
+	
+	if (empty($url))
+	    $url = 'http://api.flickr.com/services/feeds/photos_public.gne?id=25087033@N04&lang=en-us&format=atom';
+        
+	if (empty($images))
+	    $images = 0;
+	
+        ?>
+            <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+	    <p><label for="<?php echo $this->get_field_id('url'); ?>"><?php _e('Feed Url:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('url'); ?>" name="<?php echo $this->get_field_name('url'); ?>" type="text" value="<?php echo $url; ?>" /></label></p>
+	    <p><label for="<?php echo $this->get_field_id('images'); ?>"><?php _e('# of Images:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('images'); ?>" name="<?php echo $this->get_field_name('images'); ?>" type="text" value="<?php echo $images; ?>" /></label></p>
+        <?php 
+    }
+
+} // class RSSImages
+
+
+add_action('widgets_init', create_function('', 'return register_widget("RSSImages");')); // register RSSImages widget
+
+
+?>
