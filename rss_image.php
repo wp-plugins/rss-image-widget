82539bada2dbfc27ddca7fc0739ef8269438effa
zackdesign <zackdesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-16 04:27:51 +0000

    RSS Image Widget 1.3
    git-svn-id: https://plugins.svn.wordpress.org/rss-image-widget/tags/1.3@206337 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rss_image.php
+++ rss_image.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: RSS Images
-Plugin URI: http://www.zackdesign.biz/wp-plugins/40
+Plugin URI: http://www.zackdesign.biz/category/wp-plugins/rss-image-widget
 Description: RSS Image display using SimplePie
 Author: Isaac Rowntree
 Version: 1.3
